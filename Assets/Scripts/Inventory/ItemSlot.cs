f9cfbd6bf9dd63aa5dc0d8e40dd5a24247c47f8f
vlg425 <114525880+vlg425@users.noreply.github.com>
2025-11-22 23:15:52 -0500

    added TextTMP files

--- Assets/Scripts/Inventory/ItemSlot.cs
+++ Assets/Scripts/Inventory/ItemSlot.cs
@@ -1,14 +1,20 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class ItemSlot : MonoBehaviour, IDropHandler
 {
+    [SerializeField] TMP_Text itemSlotDebugText;
+    
+    int x,y;
+    string slot;
+    
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log("OnDrop");
         if (eventData.pointerDrag != null)
         {
-            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = this.GetComponent<RectTransform>().anchoredPosition;
+            eventData.pointerDrag.GetComponent<RectTransform>().position = this.GetComponent<RectTransform>().position;
         }
     }
 }
